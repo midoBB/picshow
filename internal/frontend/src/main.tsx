5750147b7ce89875f94538944a804019e2628ac2
Mohamed Hamdi <haamdi@outlook.com>
2024-07-12 17:02:06 +0100

    chore(frontend): remove React Query Devtools
    Remove React Query devtools as it is not a production dependency and only used as development aid.

--- internal/frontend/src/main.tsx
+++ internal/frontend/src/main.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import App from "./App.tsx";
-import "./index.css";
+import App from "@/App.tsx";
+import "@/index.css";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
