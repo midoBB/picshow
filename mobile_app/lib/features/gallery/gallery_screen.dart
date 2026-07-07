fd86011cda8caa54b91b286d0191fd2cc254c58d
Mohamed Hamdi <haamdi@outlook.com>
2026-07-07 08:27:13 +0100

    refactor(gallery): remove unused date formatting from gallery screen
    - Remove unused import of intl package in
      mobile_app/lib/features/gallery/gallery_screen.dart.
    - Remove the description field mapping in the GalleryItem creation
      within mobile_app/lib/features/gallery/gallery_screen.dart, as the
      formatted date string is no longer required for the gallery item
      display logic.

--- mobile_app/lib/features/gallery/gallery_screen.dart
+++ mobile_app/lib/features/gallery/gallery_screen.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/intl.dart';
 import 'package:k_gallery/k_gallery.dart';
 // ignore: implementation_imports
 import 'package:k_gallery/src/bloc/gallery_bloc.dart' as k_gallery_bloc;
@@ -37,7 +36,6 @@ class GalleryScreen extends ConsumerWidget {
           ? GalleryItemType.video
           : GalleryItemType.image,
       thumbnailUrl: api.thumbnailUrl(file.id),
-      description: DateFormat.yMMMd().add_jm().format(file.createdAt.toLocal()),
     );
   }
 
